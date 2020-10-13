bdf2864f5439b23d32d5b5ebbeb5e117fe2fdfbd
nurtt0949 <nurtt0949@wrdsb.ca>
2020-10-13 14:30:46 +0000

    Prints factors

--- Main.java
+++ Main.java
@@ -1,15 +1,36 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *Prints Factors 
+ * @author Tolga Nurtekin
  */
 public class Main {
 
+  public static void factors(int integer){
+    //create a loop to see factors
+    for(int i = 1; i <= integer; i++){
+     if(integer % i == 0){
+       System.out.println(i);
+     }
+    
+    }
+  }
+  
+  
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create a Scanner
+    Scanner input = new Scanner(System.in);
+    //ask user for integer
+    System.out.println("Enter Number");
+    //declare a variable for users Number
+    int integer = input.nextInt();
+    factors(integer);
+
+
+    
     
   }
 }
